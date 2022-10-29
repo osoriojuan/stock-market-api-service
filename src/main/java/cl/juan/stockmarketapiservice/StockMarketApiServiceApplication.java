7e3b1915fdce0655f1e0ed8f41c075eb4c808514
Juan Osorio Barrera <ext_josoriob@falabella.cl>
2022-10-29 14:03:52 -0300

    init commit

---
+++ src/main/java/cl/juan/stockmarketapiservice/StockMarketApiServiceApplication.java
@@ -0,0 +1,13 @@
+package cl.juan.stockmarketapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMarketApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMarketApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/juan/stockmarketapiservice/StockMarketApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.juan.stockmarketapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
