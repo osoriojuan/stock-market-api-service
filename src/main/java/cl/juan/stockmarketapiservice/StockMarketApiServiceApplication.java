95eb7971288f9a4f2e51333a966f12051a199eed
Juan Osorio Barrera <ext_josoriob@falabella.cl>
2022-11-01 05:52:42 -0300

    fix port

--- src/main/java/cl/juan/stockmarketapiservice/StockMarketApiServiceApplication.java
+++ src/main/java/cl/juan/stockmarketapiservice/StockMarketApiServiceApplication.java
@@ -1,5 +1,6 @@
 package cl.juan.stockmarketapiservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,9 +9,11 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableCaching
+@Slf4j
 public class StockMarketApiServiceApplication {
 
     public static void main(String[] args) {
+        log.info("Starting v1 app");
         SpringApplication.run(StockMarketApiServiceApplication.class, args);
     }
 
